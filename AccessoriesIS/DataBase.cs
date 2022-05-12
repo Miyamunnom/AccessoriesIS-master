2fad0a8d423c69569852242bfd9219efb06d3112
ximik <ximik1357@gmail.com>
2022-05-12 14:20:14 +0300

    Добавьте файлы проекта.

---
+++ AccessoriesIS/DataBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessoriesIS
+{
+    class DataBase
+    {
+        private static Entities1 _context;
+        public static Entities1 GetContext()
+        {
+            if (_context == null)
+                _context = new Entities1();
+            return _context;
+        }
+    }
+}
---
+++ AccessoriesIS/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AccessoriesIS
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_auth_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(loginTextBox.Text))
+            {
+                if (!string.IsNullOrEmpty(passwordTextBox.Password))
+                {
+                    IQueryable<Authorization> autorizstion_list = DataBase.GetContext().Authorization.Where(p => p.Login == loginTextBox.Text && p.Password == passwordTextBox.Password);
+                    if (autorizstion_list.Count() == 1)
+                        MessageBox.Show("Добро пожаловать.");
+                    else MessageBox.Show("Неверный логин или пароль.");
+
+                }
+            }
+        }
+    }
+}
